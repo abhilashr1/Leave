b34fabe0b4c9c17f9df00f641e04c22db9dfa33f
Abhilash Rejanair <1995ad@gmail.com>
2018-01-28 13:30:31 +0530

    Trying multiple contexts

---
+++ Leave/Migrations/20180128074017_Admins.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Leave.Migrations
+{
+    public partial class Admins : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Leave/Models/AdminModelContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Leave.Models
+{
+    public class AdminModelContext: DbContext
+    {
+        public DbSet<AdminModel> LeaveRequest { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Admins.db");
+        }
+    }
+}
