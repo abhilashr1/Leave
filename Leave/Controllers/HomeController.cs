19aa415005c66e1ad8dfad18533520b45018a1b3
Abhilash Rejanair <1995ad@gmail.com>
2018-01-28 19:24:38 +0530

    Added about in navbar.

--- Leave/Controllers/HomeController.cs
+++ Leave/Controllers/HomeController.cs
@@ -54,5 +54,10 @@ namespace Leave.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
