927ca7900311e57935e2bd5b06e41ca8f18e6cfb
Abhilash Rejanair <1995ad@gmail.com>
2018-01-29 17:22:05 +0530

    Before Logging

--- Leave/Startup.cs
+++ Leave/Startup.cs
@@ -24,6 +24,16 @@ namespace Leave
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["Production:SqliteConnectionString"];
+
+            services.AddDbContext<LeaveRequestContext>(options =>
+                options.UseSqlite(connection)
+            );
+
+            services.AddDbContext<AdminModelContext>(options =>
+                options.UseSqlite(connection)
+            );
+
             services.AddMvc();
             services.AddSession();
         }
